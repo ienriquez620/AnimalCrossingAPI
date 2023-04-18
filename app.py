98d7b43756f59704e916ef41dbfddc61e946cd33
Your Name <you@example.com>
2023-04-18 11:04:08 -0400

    UPDATE2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import requests
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField, SelectField
+from wtforms import IntegerField, SubmitField, SelectField, StringField
 from wtforms.validators import NumberRange
 
 import main_functions
@@ -12,11 +12,13 @@ import main_functions
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 
+
 # Choose Weather: Rainy, Snowy, Sunny
 # Choose by Hour: 1-24
 
 
 class Music(FlaskForm):
+    name = StringField("Enter Your Name:")
     hour = IntegerField("Select an Hour", validators=[NumberRange(min=0, max=24, message='Invalid length')])
     weather = SelectField("Select the Weather", choices=["Sunny",
                                                          "Rainy",
@@ -25,19 +27,39 @@ class Music(FlaskForm):
     submit = SubmitField('Submit')
 
 
+# class Villager(FlaskForm):
+#     hour = IntegerField("Select an Hour", validators=[NumberRange(min=0, max=24, message='Invalid length')])
+#     weather = SelectField("Select the Weather", choices=["Sunny",
+#                                                          "Rainy",
+#                                                          "Snowy"
+#                                                          ])
+#     submit = SubmitField('Submit')
+
 def request_music():
-    url = "http://acnhapi.com/v1a/backgroundmusic/"
-    response = requests.get(url).json()  # making the request
+    music_url = "http://acnhapi.com/v1a/backgroundmusic/"
+    response = requests.get(music_url).json()  # making the request
     main_functions.save_to_file(response, "bgm.json")  # saving the response to a JSON file
     return response
 
+
 bgm_info = main_functions.read_from_file("bgm.json")  # reading from the newly created JSON file
+
+
+# def request_villager():
+#     villager_url = "https://acnhapi.com/v1/villagers/"
+#     response2 = requests.get(villager_url).json()  # making the request
+#     main_functions.save_to_file(response2, "villager.json")  # saving the response to a JSON file
+#     return response2
+#
+#
+# villager_info = main_functions.read_from_file("villager.json")  # reading from the newly created JSON file
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = Music()
     if request.method == "POST":
+        name_entered = request.form['name']
         hour_entered = int(request.form['hour'])
         weather_entered = request.form['weather']
 
@@ -48,12 +70,11 @@ def index():
         music_list = []
         for i in bgm_music:
             if i["hour"] == hour_entered and i["weather"] == weather_entered:
-                music_list.append((i["file-name"], i["music_uri"], i["hour"],i["weather"]))
+                music_list.append((i["file-name"], i["music_uri"], i["hour"], i["weather"]))
                 print(f"Match found: {i}")
-            # music_list.append((i["file-name"], i["music_uri"]))
 
-
-        return render_template("bgm_info.html", music_list=music_list, hour_entered=hour_entered,
+        return render_template("bgm_info.html", name_entered=name_entered, music_list=music_list,
+                               hour_entered=hour_entered,
                                weather_entered=weather_entered)
     return render_template('ACNH.html', form=form)
 
