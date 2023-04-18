5cf4c5a83d05208e076b7a04fdc80d28ee8e53c7
Your Name <you@example.com>
2023-04-18 09:35:15 -0400

    UPDATE

--- app.py
+++ app.py
@@ -1,49 +1,62 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField, SelectField, validators
+import json
 import os
-from wtforms.validators import NumberRange
-import main_functions
+
 import requests
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField, SelectField
+from wtforms.validators import NumberRange
+
+import main_functions
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 
-
-url = "http://acnhapi.com/v1a/backgroundmusic/"
-response = requests.get(url).json()  # making the request
-main_functions.save_to_file(response, "bgm.json")  # saving the response to a JSON file
-
-#Choose Weather: Rainy, Snowy, Sunny
-#Choose by Hour: 1-24
-
-bgm_info = main_functions.read_from_file("bgm.json") # reading from the newly created JSON file
-lst = [] # initializing an empty list
-for i in ["hour"]:
-    if i["hour"] == hour:
-        lst.append(i["music_uri"])
+# Choose Weather: Rainy, Snowy, Sunny
+# Choose by Hour: 1-24
 
 
 class Music(FlaskForm):
-     hour = IntegerField("Select an Hour",validators=[NumberRange(min=1, max=24, message='Invalid length')])
-     weather = SelectField("Select the Weather", choices=[("Sunny"),
-                                                          ("Rainy"),
-                                                          ("Snowy")
-                                                          ])
-     submit = SubmitField('Submit')
+    hour = IntegerField("Select an Hour", validators=[NumberRange(min=0, max=24, message='Invalid length')])
+    weather = SelectField("Select the Weather", choices=["Sunny",
+                                                         "Rainy",
+                                                         "Snowy"
+                                                         ])
+    submit = SubmitField('Submit')
+
+
+def request_music():
+    url = "http://acnhapi.com/v1a/backgroundmusic/"
+    response = requests.get(url).json()  # making the request
+    main_functions.save_to_file(response, "bgm.json")  # saving the response to a JSON file
+    return response
+
+bgm_info = main_functions.read_from_file("bgm.json")  # reading from the newly created JSON file
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = Music()
-    if form.validate_on_submit():
-         hour_entered = form.hour.data
-         weather_entered = form.weather.data
-         list_of_bgm = get_list_of_bgm(hour_entered, weather_entered,url)
-         return render_template("bgm_info.html",list_of_bgm=list_of_bgm,length=len(list_of_bgm),weather_entered=weather,hour_entered=hour)
+    if request.method == "POST":
+        hour_entered = int(request.form['hour'])
+        weather_entered = request.form['weather']
+
+        with open('bgm.json', 'r') as f:
+            bgm_music = json.load(f)
+
+        bgm_music = request_music()
+        music_list = []
+        for i in bgm_music:
+            if i["hour"] == hour_entered and i["weather"] == weather_entered:
+                music_list.append((i["file-name"], i["music_uri"], i["hour"],i["weather"]))
+                print(f"Match found: {i}")
+            # music_list.append((i["file-name"], i["music_uri"]))
+
+
+        return render_template("bgm_info.html", music_list=music_list, hour_entered=hour_entered,
+                               weather_entered=weather_entered)
     return render_template('ACNH.html', form=form)
+
 
 if __name__ == '__main__':
     app.run()
-    
-
